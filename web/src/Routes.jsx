d3c014870ad212c15342b3e81411874ee8ab8cb8
asxyzp <asxyzp@gmail.com>
2023-08-21 18:08:14 +0530

    Updating routes component

--- web/src/Routes.jsx
+++ web/src/Routes.jsx
@@ -1,27 +1,33 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
-// directories are supported, and should be uppercase. Each subdirectory will be
-// prepended onto the component name.
-//
-// Examples:
-//
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
-// 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { useRecoilState } from 'recoil'
 
 import { Set, Router, Route } from '@redwoodjs/router'
 
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
+import ModalRouter from './components/ModalRouter/ModalRouter'
+import { darkModeAtom } from './contexts/atoms'
+import DarkTheme from './styles/darkTheme'
+import LightTheme from './styles/lightTheme'
+
 const Routes = () => {
+  // GETTING ATOMIC STATES
+  const [isDarkMode] = useRecoilState(darkModeAtom)
+
   return (
-    <Router>
-      <Set wrap={ScaffoldLayout} title="Invoices" titleTo="invoices" buttonLabel="New Invoice" buttonTo="newInvoice">
-        <Route path="/invoices/new" page={InvoiceNewInvoicePage} name="newInvoice" />
-        <Route path="/invoices/{id:Int}/edit" page={InvoiceEditInvoicePage} name="editInvoice" />
-        <Route path="/invoices/{id:Int}" page={InvoiceInvoicePage} name="invoice" />
-        <Route path="/invoices" page={InvoiceInvoicesPage} name="invoices" />
-      </Set>
-      <Route notfound page={NotFoundPage} />
-    </Router>
+    <ThemeProvider theme={isDarkMode ? DarkTheme : LightTheme}>
+      <CssBaseline />
+      <ModalRouter />
+      <Router>
+        <Set wrap={ScaffoldLayout}>
+          <Route path="/invoices/new" page={InvoiceNewInvoicePage} name="newInvoice" />
+          <Route path="/invoices/{id:Int}/edit" page={InvoiceEditInvoicePage} name="editInvoice" />
+          <Route path="/invoices/{id:Int}" page={InvoiceInvoicePage} name="invoice" />
+          <Route path="/" page={InvoiceInvoicesPage} name="invoices" />
+        </Set>
+        <Route notfound page={NotFoundPage} />
+      </Router>
+    </ThemeProvider>
   )
 }
 
