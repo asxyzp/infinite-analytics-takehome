29bb2aaff46024d7c332e7f056ae2c789e59d432
asxyzp <asxyzp@gmail.com>
2023-08-21 18:09:20 +0530

    Updating scffolding layout

--- web/src/layouts/ScaffoldLayout/ScaffoldLayout.jsx
+++ web/src/layouts/ScaffoldLayout/ScaffoldLayout.jsx
@@ -1,25 +1,11 @@
-import { Link, routes } from '@redwoodjs/router'
 import { Toaster } from '@redwoodjs/web/toast'
 
-const ScaffoldLayout = ({
-  title,
-  titleTo,
-  buttonLabel,
-  buttonTo,
-  children,
-}) => {
+const ScaffoldLayout = ({ children }) => {
   return (
     <div className="rw-scaffold">
       <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
       <header className="rw-header">
-        <h1 className="rw-heading rw-heading-primary">
-          <Link to={routes[titleTo]()} className="rw-link">
-            {title}
-          </Link>
-        </h1>
-        <Link to={routes[buttonTo]()} className="rw-button rw-button-green">
-          <div className="rw-button-icon">+</div> {buttonLabel}
-        </Link>
+        <h1 className="rw-heading rw-heading-primary"></h1>
       </header>
       <main className="rw-main">{children}</main>
     </div>
