dfd17c7722546cde0d0389a0ba46ff40ed31578b
asxyzp <asxyzp@gmail.com>
2023-08-22 02:42:07 +0530

    Updating default and placeholder values

--- web/src/components/Invoice/InvoiceForm/InvoiceForm.jsx
+++ web/src/components/Invoice/InvoiceForm/InvoiceForm.jsx
@@ -1,5 +1,6 @@
 import { Publish, Visibility } from '@mui/icons-material'
 import { Box, Typography, styled } from '@mui/material'
+import { v4 as uuidv4 } from 'uuid'
 
 import { Form } from '@redwoodjs/forms'
 
@@ -7,6 +8,7 @@ import Alert from 'src/components/Alert/Alert'
 import Button from 'src/components/Button/Button'
 import Fieldset from 'src/components/Fieldset/Fieldset'
 import Input from 'src/components/Input/Input'
+
 import './invoiceForm.css'
 
 const formatDatetime = (value) => {
@@ -50,6 +52,29 @@ const CustomBox = styled(Box)(({ theme }) => ({
 }))
 
 const InvoiceForm = (props) => {
+  // SETTING LOCAL VARIABLES
+  // SETTING DEFAULT VALUES
+  const defaultInvoice = {
+    title: `invoice_${new Date().getDate()}_${new Date().getMonth()}_${new Date().getFullYear()}`,
+    description: `Invoice generated on ${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
+    paymentTerms: `The due amount can be paid to the Random Bank of India's account no. RBOI${uuidv4()
+      .split('-')[0]
+      .toUpperCase()} or through the following link: https://random-payment-app.com/pay/${uuidv4()}`,
+    dueDate: new Date(Date.now() + 864000000).toISOString(),
+  }
+  const defaultSeller = {
+    name: 'Rajesh Kumar',
+    email: '+91 9876543210',
+    phone: 'rajesh.kumar@example.com',
+    address: '45 Patel Nagar, New Delhi, India',
+  }
+  const defaultBuyer = {
+    name: 'Priya Sharma',
+    email: '+91 8765432109',
+    phone: 'priya.sharma@example.com',
+    address: '22 Gandhi Road, Mumbai, India',
+  }
+
   const onSubmit = (data) => {
     props.onSave(data, props?.invoice?.id)
   }
@@ -67,7 +92,9 @@ const InvoiceForm = (props) => {
         <Fieldset legend="Basic invoice details">
           <Input
             type="text"
-            defaultValue={props.invoice?.title}
+            defaultValue={
+              props.invoice ? props.invoice.title : defaultInvoice.title
+            }
             label="Invoice title"
             size="small"
             margin="medium"
@@ -76,18 +103,26 @@ const InvoiceForm = (props) => {
           />
           <Input
             type="text"
-            defaultValue={props.invoice?.description}
+            defaultValue={
+              props.invoice
+                ? props.invoice.description
+                : defaultInvoice.description
+            }
             label="Invoice description"
             size="small"
             margin="medium"
             multiline={true}
-            minRows={4}
+            minRows={1}
             fullWidth
             required
           />
           <Input
             type="text"
-            defaultValue={props.invoice?.paymentTerms}
+            defaultValue={
+              props.invoice
+                ? props.invoice.paymentTerms
+                : defaultInvoice.paymentTerms
+            }
             label="Terms of payment"
             size="small"
             margin="medium"
@@ -99,7 +134,9 @@ const InvoiceForm = (props) => {
           <Input
             label="Due date"
             type="datetime-local"
-            defaultValue={formatDatetime(props.invoice?.dueAt)}
+            defaultValue={formatDatetime(
+              props.invoice ? props.invoice.dueAt : defaultInvoice.dueDate
+            )}
             validation={{ required: true }}
             size="small"
             margin="medium"
@@ -113,6 +150,7 @@ const InvoiceForm = (props) => {
             type="text"
             label="Seller's name"
             defaultValue={props.invoice?.sellerName}
+            placeholder={defaultSeller.name}
             size="small"
             margin="medium"
             fullWidth
@@ -121,6 +159,7 @@ const InvoiceForm = (props) => {
           <Input
             type="tel"
             defaultValue={props.invoice?.sellerPhone}
+            placeholder={defaultSeller.phone}
             label="Seller's phone number"
             size="small"
             margin="medium"
@@ -129,6 +168,7 @@ const InvoiceForm = (props) => {
           <Input
             type="email"
             label="Seller's email address"
+            placeholder={defaultSeller.email}
             defaultValue={props.invoice?.sellerEmail}
             size="small"
             margin="medium"
@@ -136,6 +176,7 @@ const InvoiceForm = (props) => {
           />
           <Input
             type="text"
+            placeholder={defaultSeller.address}
             defaultValue={props.invoice?.sellerAddress}
             label="Seller's address"
             multiline={true}
@@ -151,6 +192,7 @@ const InvoiceForm = (props) => {
             type="text"
             label="Buyer's name"
             defaultValue={props.invoice?.buyerName}
+            placeholder={defaultBuyer.name}
             validation={{ required: true }}
             size="small"
             margin="medium"
@@ -160,6 +202,7 @@ const InvoiceForm = (props) => {
           <Input
             type="tel"
             defaultValue={props.invoice?.buyerPhone}
+            placeholder={defaultBuyer.phone}
             label="Buyer's phone number"
             size="small"
             margin="medium"
@@ -168,6 +211,7 @@ const InvoiceForm = (props) => {
           <Input
             type="email"
             defaultValue={props.invoice?.buyerEmail}
+            placeholder={defaultBuyer.email}
             size="small"
             margin="medium"
             label="Buyer's email address"
@@ -176,6 +220,7 @@ const InvoiceForm = (props) => {
           <Input
             type="text"
             defaultValue={props.invoice?.buyerAddress}
+            placeholder={defaultBuyer.address}
             label="Buyer's address"
             size="small"
             margin="medium"
@@ -185,7 +230,7 @@ const InvoiceForm = (props) => {
           />
         </Fieldset>
 
-        <Fieldset legend="Line items">abcd</Fieldset>
+        <Fieldset legend="Line items"></Fieldset>
 
         <Box className="invoice-form-buttons">
           <Button
